0b4652be34919af3a1d2283806ec5bd41776c7cd
masudpy <masudcse74@gmail.com>
2022-06-19 21:48:57 +0600

    Some changes

--- App_Login/forms.py
+++ App_Login/forms.py
@@ -1,10 +1,21 @@
-from dataclasses import field
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from dataclasses import field
+from tkinter import Widget
+from django import forms
+from django.contrib.auth.forms import UserCreationForm,UsernameField
+from django.utils.translation import gettext, gettext_lazy as _
 from django.contrib.auth.models import User
 
+
 class CreateNewUser(UserCreationForm):
-    email = forms.EmailField(required=True)
-    class Meat:
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    # email = forms.EmailField(required=True)
+    class Meta:
         model = User
-        fields = ('email','username','password1','password2')
+        fields = ('username','email')
+        labels = {'email': 'Email Address'}
+        widgets = {
+            'username':forms.TextInput(attrs={'class':'form-control'}),
+            'email':forms.TextInput(attrs={'class':'form-control'}),
+        }
