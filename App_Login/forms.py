f32fad91d757484402dcb2bd895be80f29ed0e45
masudpy <masudcse74@gmail.com>
2022-06-27 02:51:56 +0600

    some update

--- App_Login/forms.py
+++ App_Login/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from dataclasses import field
-from tkinter import Widget
 from django import forms
 from django.contrib.auth.forms import UserCreationForm,UsernameField
-from django.utils.translation import gettext, gettext_lazy as _
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import  gettext_lazy as _
 from django.contrib.auth.models import User
+from App_Login.models import UserProfile
 
 
 class CreateNewUser(UserCreationForm):
@@ -19,3 +19,22 @@ class CreateNewUser(UserCreationForm):
             'username':forms.TextInput(attrs={'class':'form-control'}),
             'email':forms.TextInput(attrs={'class':'form-control'}),
         }
+
+
+class LoginForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus':True, 'class':'form-control'}))
+    password = forms.CharField(label=_('Password'),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'current-password', 'class':'form-control'}))
+
+
+class EditProfile(forms.ModelForm):
+    dob = forms.DateField(widget=forms.TextInput(attrs={'type':'date'}))
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # My_Social_Project With Django
+App_Login
+App_Posts
+media
+static
+templates
+INSTALLED_APPS =['App_Login',
+		 'App_Posts',
+    		 'crispy_forms',]
+
+
+
+
 
