72bb9a3d36ae76f1f082b264f8386903eff08aad
masudcse100 <masudcse74@gmail.com>
2022-09-22 22:48:29 +0600

    Search profile & Edit own profile

--- App_Posts/views.py
+++ App_Posts/views.py
@@ -1,14 +1,22 @@
+from cmath import log
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.contrib.auth.decorators import login_required
 from App_Login.models import UserProfile
 from django.contrib.auth.models import User
-
+from App_Posts.models import Post
 # Create your views here.
 @login_required
 def home(request):
     if request.method == "GET":
         search = request.GET.get('search', '')
         result = User.objects.filter(username__icontains=search)
-    return render(request,'App_Posts/home.html', context={'title':'Home', 'search':search, 'result':result})
+        posts = Post.objects.all()
+        context={
+            'title':'Home',
+            'search':search,
+            'result':result,
+            'posts':posts
+            }
+    return render(request,'App_Posts/home.html', context)
 
